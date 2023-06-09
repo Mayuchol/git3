9d47a865b366da59f7d7790575e3fa8ca268d96b
Maksim <maks.yurchenko.01@ukr.net>
2023-06-09 13:44:40 +0300

    Main second commit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Init");
         System.out.println("First");
+        System.out.println("Second");
     }
 }
